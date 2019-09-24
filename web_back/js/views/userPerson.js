176f131c5198b5f518122f7ad4ad5b4877a47b1e
Old-du <1990905880@qq.com>
2019-09-24 10:36:22 +0800

    文章 个人中心部分

--- web_back/js/views/userPerson.js
+++ web_back/js/views/userPerson.js
@@ -1,25 +1,38 @@
-$(function() {
-    user.userPerson(function(res) {
+$(function () {
+    user.userPerson(function (res) {
         $('#inputEmail3').val(res.data.username);
         $('#inputEmail4').val(res.data.nickname);
         $('#inputEmail6').val(res.data.password);
         $('#inputEmail5').val(res.data.email);
     })
 })
+var tmpFilePath;
 //选择图片部分
-$('#exampleInputFile').change(function() {
+$('#exampleInputFile').change(function () {
     // console.dir(this);
 
     // console.log(this.files[0].name)
     // $('.user_pic').attr('src','images/'+this.files[0].name)
-    var file=this.files[0];
-    var tmpFilePath = URL.createObjectURL(file);
-    $('.user_pic').attr('src',tmpFilePath)
+    var file = this.files[0];
+    tmpFilePath = URL.createObjectURL(file);
+    $('.user_pic').attr('src', tmpFilePath);
 })
 //点击修改按钮部分
-$('#PersonEdit').click(function() {
-    var fd=new FormData($('#MyForm')[0])
-    user.userPerson_edit(fd,function(res) {
-        console.log('测试');
+$('#PersonEdit').click(function () {
+    var fd = new FormData($('#MyForm')[0])
+    user.userPerson_edit(fd, function (res) {
+        if (res.code == 200) {
+            user.getuser(function(res) {
+                // console.log(tmpFilePath)
+                if(res.code==200) {
+                    console.log(window.document.getElementById("indexImg"))
+                    // $(".user_info img").prop('src',res.data.user_pic)
+                    // $(".user_info span").html(res.data.nickname)
+                    // window.parent.document.querySelector("#indexImg").src=tmpFilePath
+                }  
+              }) 
+           
+        }
     })
 })
+   
